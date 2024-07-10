6f342601fc1259c56889c2053c5e3d49ff5911f1
Your Name <you@example.com>
2024-07-11 00:05:17 +0200

    Fixed navbar alignment

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,33 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">Dubai Project</Link>
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          Dubai Project
+        </div>
+        <ul className="navbar-menu">
+          <li className="navbar-item">
+            <Link to="/" className="navbar-link">Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/services" className="navbar-link">Services</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/real-estate" className="navbar-link">Real Estate</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/medical-vacation" className="navbar-link">Medical Vacation</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/business-solutions" className="navbar-link">Business Solutions</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/about-us" className="navbar-link">About Us</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/contact" className="navbar-link">Contact</Link>
+          </li>
+        </ul>
       </div>
-      <div className="hamburger" onClick={toggleMenu}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
-      <ul className={`navbar-menu ${menuOpen ? 'open' : ''}`}>
-        <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
-        <li><Link to="/services" onClick={toggleMenu}>Services</Link></li>
-        <li><Link to="/real-estate" onClick={toggleMenu}>Real Estate</Link></li>
-        <li><Link to="/medical-vacation" onClick={toggleMenu}>Medical Vacation</Link></li>
-        <li><Link to="/business-solutions" onClick={toggleMenu}>Business Solutions</Link></li>
-        <li><Link to="/about-us" onClick={toggleMenu}>About Us</Link></li>
-        <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
-      </ul>
     </nav>
   );
 };
